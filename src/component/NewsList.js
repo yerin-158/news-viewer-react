5611e41440f997c9bc52a92f4ef756a02ebf4987
yerin-158 <hanblueblue@gmail.com>
2020-10-20 22:43:13 +0900

    커스텀 hooks을 이용하여 promise 비동기 처리

--- src/component/NewsList.js
+++ src/component/NewsList.js
@@ -2,31 +2,38 @@ import React, { useEffect, useState } from 'react';
 import '../css/NewsList.scss';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsList = props => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
+    // const [articles, setArticles] = useState(null);
+    // // const [loading, setLoading] = useState(false);
     const {selectedCategory} = props;
+    const [loading, response, error] = usePromise(() => {
+        const query = selectedCategory === 'all' ? '' : `&category=${selectedCategory}`;
+        return axios.get(
+                            `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c82b95a3df2a471eb5bde466f14dc8eb`
+                        );
+    }, [selectedCategory]);
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = selectedCategory === 'all' ? '' : `&category=${selectedCategory}`;
-                const response = await axios.get(
-                    `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c82b95a3df2a471eb5bde466f14dc8eb`
-                );
-                // await으로 기다리게 하고 response 받으면 setArticle 호출
+    //     const fetchData = async () => {
+    //         setLoading(true);
+    //         try {
+    //             const query = selectedCategory === 'all' ? '' : `&category=${selectedCategory}`;
+    //             const response = await axios.get(
+    //                 `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c82b95a3df2a471eb5bde466f14dc8eb`
+    //             );
+    //             // await으로 기다리게 하고 response 받으면 setArticle 호출
             
-                setArticles(response.data.articles);
-            } catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        }
-        fetchData();
-    }, [selectedCategory]); // 선택된 카테고리 바뀌면 재수행
+    //             setArticles(response.data.articles);
+    //         } catch (e) {
+    //             console.log(e);
+    //         }
+    //         setLoading(false);
+    //     }
+    //     fetchData();
+    // }, [selectedCategory]); // 선택된 카테고리 바뀌면 재수행
 
     // fetchData 비동기로 던져놓고 아래로 계속 수행함.
 
@@ -38,9 +45,15 @@ const NewsList = props => {
         )
     }
 
-    if (!articles) {
+    if (!response) {
         return null;
     }
+
+    if (error) {
+        return <div className="NewsList">에러 발생!!</div>
+    }
+
+    const {articles} = response.data;
 
     return (
         <div className="NewsList">
